53a93738be84d0cc83452a8f5824732976cca57d
michelladambento <michelladamb@gmail.com>
2025-12-27 18:57:14 -0300

    Estudos

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using exemplofundamentos.Models;
+
+Pessoa p = new Pessoa();
+p.Idade = 38;
+p.Nome = "Michell Adam Bento";
+p.Apresentar();
